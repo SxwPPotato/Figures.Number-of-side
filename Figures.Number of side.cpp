214311ee7fa4da4a4c3c39540d7bbbbab862b9f6
Vitalya <vitalyabes.vb@gmail.com>
2023-10-31 11:09:50 +0300

    Initial commit

---
+++ Figures.Number of side.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+class Figures {
+protected:
+    int Number_of_sides;
+public:
+    Figures() {
+        Number_of_sides = 0;
+    }
+    void Show_sides() {
+        std::cout << Number_of_sides << "\n";
+    }
+};
+
+class Triangle : public Figures {
+public:
+    Triangle() {
+        Number_of_sides = 3;
+    }
+};
+
+class Quadrangle : public Figures {
+public:
+    Quadrangle() {
+        Number_of_sides = 4;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    Figures figure;
+    Triangle triangle;
+    Quadrangle quadrangle;
+
+    std::cout << "Количество сторон\n";
+    std::cout << "Фигура: ";
+    figure.Show_sides();
+    std::cout << "Треугольник: ";
+    triangle.Show_sides();
+    std::cout << "Четырёхугольник: ";
+    quadrangle.Show_sides();
+
+    return 0;
+}
